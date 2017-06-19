1ae73f1384c3c970ad94bced5bbc4ba3fc9740d2
liubin <752791962@qq.com>
2017-06-19 15:05:15 +0800

    1

--- js/m.touch.js
+++ js/m.touch.js
@@ -6,7 +6,7 @@ $(function(){
 		$body.find('div.search_input_con').show(0);
 		$body.find('div.search_input_con form.h_search_form input').trigger('click').focus();
 	});
-	$('div.search_input_con a.search_input_close').tap(function(){
+	$('div.search_input_con a.search_input_close').click(function(){
 		var $body = $(this).parents('body');
 		$body.find('div.search_input_con').hide(0);
 		$body.find('header.head_index, div.main_container, footer').show(0);
