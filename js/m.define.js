8e6e58be9635249db7cca964dfeb201f2ccc1092
liubin <752791962@qq.com>
2017-06-19 17:35:35 +0800

    123

---
+++ js/m.define.js
@@ -0,0 +1,11 @@
+	//搜索检测是否为空
+	function formCheck(){
+		var $form = $('.h_search_form');
+		var val = $form.find('input').val();
+		if (val == '' || val.length == 0) {
+			return false;
+		}else { 
+			return true;
+		}
+	}
+	//
