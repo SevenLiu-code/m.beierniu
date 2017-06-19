58db28ff5cf0c7589d9198e18f33a6ca6259e8b5
liubin <752791962@qq.com>
2017-06-19 11:23:33 +0800

    home

--- js/m.touch.js
+++ js/m.touch.js
@@ -2,15 +2,19 @@
 $(function(){
 	$('div.h_search_box').tap(function(){
 		var $body = $(this).parents('body');
-		$body.find('header.head_index, div.main_container, footer').hide();
-		$body.find('div.search_input_con').show();
+		$body.find('header.head_index, div.main_container, footer').hide(0);
+		$body.find('div.search_input_con').show(0);
 		$body.find('div.search_input_con form.h_search_form input').focus();
 	});
 	$('div.search_input_con a.search_input_close').tap(function(){
 		var $body = $(this).parents('body');
-		$body.find('div.search_input_con').hide();
-		$body.find('header.head_index, div.main_container, footer').show();
+		$body.find('div.search_input_con').hide(0);
+		$body.find('header.head_index, div.main_container, footer').show(0);
 	});
+	//清除搜索框文本
+	$('div.search_input_box span.input_clean').tap(function(){
+		$(this).parents().find('input').val('');
+	})
 })
 
 
