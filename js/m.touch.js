b6c80c526c06fda8ae972817b3fa03885dc28205
liubin <752791962@qq.com>
2017-06-20 14:45:49 +0800

    1234

--- js/m.touch.js
+++ js/m.touch.js
@@ -1,29 +1,39 @@
 // 搜索框触摸切换
 $(function(){
+ //首页	
 	$('div.h_search_box').tap(function(){
 		var $body = $(this).parents('body');
-		$body.find('header.head_index, div.main_container, footer').hide(0);
-		$body.find('div.search_input_con').show(0);
+		$body.find('header.head_index, div.main_container, footer').hide();
+		$body.find('div.search_input_con').show();
 		$body.find('div.search_input_con form.h_search_form input').trigger('click').focus();
 	});
 	$('div.search_input_con a.search_input_close').tap(function(){
 		var $body = $(this).parents('body');
-		$body.find('div.search_input_con').hide(0);
-		$body.find('header.head_index, div.main_container, footer').show(0);
+		$body.find('div.search_input_con').hide();
+		$body.find('header.head_index, div.main_container, footer').show();
 	});
 	//清除搜索框文本
 	$('div.search_input_box span.input_clean').tap(function(){
 		$(this).parents().find('input').val('');
-	})
-
-	// 条件筛选页
+	});
+	// 城市选择
+	$('.h_city').tap(function(){
+		$('.hidden_part').hide();
+		$('.city_select').show();
+	});
+	//城市选择返回
+	$('a.city_select_retrun').tap(function(){
+		$(this).parents('.city_select').hide();
+		$('.hidden_part').show();
+	});
+ // 条件筛选页
 	// 价格筛选
 	$('nav>a.filter_price, nav>a.filter_sort').tap(function(){
 		var x = $(this).attr('class').replace('filter_', '');
 		$('header.head_index').addClass('head_position');
 		$('div.filter').addClass('filter_position');
-		$('div.filter_details').removeClass('filter_details_z').show(0);
-		$('div.mask').show(0);
+		$('div.filter_details').removeClass('filter_details_z').show();
+		$('div.mask').show();
 		$('nav.filter_nav>a').removeClass('current');
 		$(this).addClass('current');
 		$(this).find('i').removeClass().addClass('icon-angle-up');
@@ -32,8 +42,8 @@ $(function(){
 	$('div.mask').tap(function(){
 		$('header.head_index').removeClass('head_position');
 		$('div.filter').removeClass('filter_position');
-		$(this).hide(0);
-		$(this).parents('body').find('.filter_details').hide(0);
+		$(this).hide();
+		$(this).parents('body').find('.filter_details').hide();
 		$('nav.filter_nav').find('a.current').removeClass('current');
 		$('nav.filter_nav>a>i').removeClass().addClass('icon-angle-down');
 	})
