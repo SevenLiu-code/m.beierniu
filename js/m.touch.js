1b247c622d721e1930c378bea85c2d74c641b804
liubin <752791962@qq.com>
2017-06-22 00:19:01 +0800

    2223

--- js/m.touch.js
+++ js/m.touch.js
@@ -142,10 +142,21 @@ $(function(){
 			//验证验证码
 			var l_price = $box.find('input.l_price').val();
 			if ( RE_l_price.test(l_price) ) {
-				
+
 			}else{
 				$box.find('p.error_text').html('输入价格有误').show();
 			}
+		}else {
+			$box.find('p.error_text').html('手机号输入有误').show();
+		}
+	})
+	$('button.sell_car_commit').tap(function(){
+		var RE_phone = /^1[34578][\d]{9}/;
+		var $box = $(this).parents('div.ask_box');
+		var phone = $box.find('input.phone').val();
+		if ( RE_phone.test(phone) ) {
+			//验证验证码
+			
 		}else {
 			$box.find('p.error_text').html('手机号输入有误').show();
 		}
